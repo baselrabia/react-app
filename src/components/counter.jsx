85f86f5d62bed234a153c9a43a5c19d9f9035fe1
baselrabia <baselrabia@gmail.com>
2020-08-05 16:12:00 +0200

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'; 
+
+class Counter extends Component {
+    state = {
+        count : 0,
+        image : "https://picsum.photos/200",
+        tags : ['tag1','tag2','tag3']
+    };
+
+
+
+    render() { 
+        let classes = this.getBadgeClass();
+        return (
+          <React.Fragment>
+            <img src={this.state.image} alt="" />
+            <br></br>
+            <span
+              style={{
+                fontSize: 50,
+                fontWeight: "bold",
+              }}
+              className={classes}
+            >
+              {this.formatCounter()}
+            </span>
+            <br></br>
+            <button className="btn btn-sm btn-secondary">Increment </button>
+            <br></br>
+           { this.renderTags()}
+          </React.Fragment>
+        );
+    }
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>there are no tags!</p>;
+        
+        return <ul>
+            {this.state.tags.map((tag) => (
+                <li key={tag}>{tag}</li>
+            ))}
+        </ul>;
+    }
+
+    getBadgeClass() {
+        let classes = "badge m-4 ";
+        classes += (this.state.count === 0) ? "badge-warning" : "badge-primary";
+        return classes;
+    }
+
+    formatCounter(){
+        const {count} = this.state;
+        return count === 0 ? "zero" : count;
+    }
+}
+ 
+export default Counter;
