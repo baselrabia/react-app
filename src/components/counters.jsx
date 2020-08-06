1b3efbdec645e25c490e2d42c1aa7275709d190e
baselrabia <baselrabia@gmail.com>
2020-08-06 16:51:08 +0200

    lifting state up to the main parent App

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,56 +2,30 @@ import React, { Component } from 'react';
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
-
+  
   render() {
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
           className="btn btn-lg btn-default m-4"
         >
           Reset
         </button>
         <br></br>
 
-        {this.state.counters.map((counter) => (
+        {this.props.counters.map((counter) => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             counter={counter}
           />
         ))}
       </div>
     );
   }
-  handleIncrement = (counter) => {
-    const counters = this.state.counters;
-    const index = counters.indexOf(counter);
-    counters[index].value++;
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
-
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters });
-  };
+ 
 }
 
 
